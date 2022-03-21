3c7922b31f70c821912ec8884685e18334b9a158
Olga <suprunovic@yahoo.com>
2022-03-21 20:40:48 +0200

    Printing all 5 arithmetic operations to the console

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,6 +3,21 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//Program that performs all arithmetic operations and prints the results to the console
+        int val1 = 5, val2 = 12;
+        //addition
+        System.out.println("The sum of two values is: " + (val1+val2));
+
+        //subtraction
+        System.out.println("val2 minus val1 equals: " + (val2 - val1));
+
+        //multiplication
+        System.out.println("val1 multiplied by val2 equals: " + (val1*val2));
+
+        //division
+        System.out.println("val2 divided by val1 equals: " + ((float)val2/(float)val1));
+
+        //modular operation
+        System.out.println("val2 % val1 equals: " + (val2 % val1));
     }
 }
